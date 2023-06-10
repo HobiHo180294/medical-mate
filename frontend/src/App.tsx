37a715e6131e5ce922741ba3ebe3e6c9b7265420
Yehor Potebenko <hitechnic.uu68@gmail.com>
2023-06-10 19:32:03 +0200

    init: set up the project front-end part

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import 'normalize.css';
+
+function App() {
+  return (
+    <>
+      <header>1</header>
+      <main className="page">1</main>
+      <footer>1</footer>
+    </>
+  );
+}
+
+export default App;
