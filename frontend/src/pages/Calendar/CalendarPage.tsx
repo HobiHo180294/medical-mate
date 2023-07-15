51e9453309ea6d54694c507f027c1b04a46a6858
Yehor Potebenko <hitechnic.uu68@gmail.com>
2023-07-15 05:46:24 +0200

    commit

--- frontend/src/pages/Calendar/CalendarPage.tsx
+++ frontend/src/pages/Calendar/CalendarPage.tsx
@@ -20,6 +20,46 @@ export const CalendarPage = () => {
     })
   );
 
+  localStorage.setItem(
+    'appointment1',
+    JSON.stringify({
+      userId: 'afa8bfee-d35e-4024-b8f1-a78cbf320882',
+      departmentName: localStorage.getItem('departmentName'),
+      date: '2023-07-20',
+      time: '11:00',
+    })
+  );
+
+  localStorage.setItem(
+    'appointment2',
+    JSON.stringify({
+      userId: 'afa8bfee-d35e-4024-b8f1-a78cbf320882',
+      departmentName: localStorage.getItem('departmentName'),
+      date: '2023-07-20',
+      time: '12:00',
+    })
+  );
+
+  localStorage.setItem(
+    'appointment3',
+    JSON.stringify({
+      userId: 'afa8bfee-d35e-4024-b8f1-a78cbf320882',
+      departmentName: localStorage.getItem('departmentName'),
+      date: '2023-07-21',
+      time: '12:00',
+    })
+  );
+
+  localStorage.setItem(
+    'appointment4',
+    JSON.stringify({
+      userId: 'afa8bfee-d35e-4024-b8f1-a78cbf320882',
+      departmentName: localStorage.getItem('departmentName'),
+      date: '2023-07-17',
+      time: '17:00',
+    })
+  );
+
   useEffect(() => {
     const paginationUL = document.querySelectorAll('ul li button');
 
@@ -52,13 +92,46 @@ export const CalendarPage = () => {
               }}
               time="10:00"
             />
-            <TimeBlock time="11:00" />
-            <TimeBlock time="12:00" />
+            <TimeBlock
+              time="11:00"
+              onClick={(event) => {
+                axios.post(
+                  'http://localhost:8080/appointment',
+                  localStorage.getItem('appointment')
+                );
+                alert(
+                  'Your appointment is successful!' + `${localStorage.getItem('appointment1')}`
+                );
+              }}
+            />
+            <TimeBlock
+              time="12:00"
+              onClick={(event) => {
+                axios.post(
+                  'http://localhost:8080/appointment',
+                  localStorage.getItem('appointment')
+                );
+                alert(
+                  'Your appointment is successful!' + `${localStorage.getItem('appointment2')}`
+                );
+              }}
+            />
             <TimeBlock time="13:00" />
             <TimeBlock time="14:00" />
             <TimeBlock time="15:00" />
             <TimeBlock time="16:00" />
-            <TimeBlock time="17:00" />
+            <TimeBlock
+              time="17:00"
+              onClick={(event) => {
+                axios.post(
+                  'http://localhost:8080/appointment',
+                  localStorage.getItem('appointment')
+                );
+                alert(
+                  'Your appointment is successful!' + `${localStorage.getItem('appointment4')}`
+                );
+              }}
+            />
             <TimeBlock time="18:00" />
           </div>
         </div>
